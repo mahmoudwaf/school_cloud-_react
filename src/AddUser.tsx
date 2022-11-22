68600b4ed0b1604e781f1517b6f04995516db65f
MAHMOUD ABO ELWAFA <mahwafa2005@gmail.com>
2022-11-22 08:21:47 +0200

    test commit

--- src/AddUser.tsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class AddUser extends Component{
-    render(){
-        return(
-            <div>Add User Component</div>
-        )
-    }
-}
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       Welcome to School Application
-      <div>Users List</div>
+      <div>Users Dashboard</div>
       <table id="usersTable">
         <thead>
           <tr>
