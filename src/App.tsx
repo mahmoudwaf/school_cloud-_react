cf9e0bdfd78c958690a0bbdf030584f5b562e242
MAHMOUD ABO ELWAFA <mahwafa2005@gmail.com>
2022-11-22 06:50:04 +0200

    fix'

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
- import './App.css';
+import './App.css';
 function App() {
-   
+
 
 
   return (
     <div className="App">
-        Welcome to School Application
-        <div>Users List</div>
-        <table id="usersTable">
+      Welcome to School Application
+      <div>Users List</div>
+      <table id="usersTable">
+        <thead>
           <tr>
-              <td>User ID</td>
-              <td>User Name</td>
-              <td>Email</td>
-              <td>Roles</td>
+            <td>User ID</td>
+            <td>User Name</td>
+            <td>Email</td>
+            <td>Roles</td>
           </tr>
+        </thead>
+        <tbody>
           <tr>
             <td>1</td>
             <td>Ahmed Sayed</td>
             <td>ahmed@gmail.com</td>
             <td>Admin</td>
           </tr>
-        </table>
-         
+        </tbody>
+      </table>
+
     </div>
   );
 }
